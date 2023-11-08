f17361dd8e7555af9d4608b843f25fbc578fdd42
kokokim <111446760+kokokim@users.noreply.github.com>
2023-11-09 00:41:55 +0900

    고쳤다

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 let tag_result=''; //태그 데이터
 let game_result='';
 
-function choosetag(){
+function choosetag(obj){
     tag_result='';
     var count=0;
     // 선택된 목록 가져오기
@@ -19,12 +19,13 @@ function choosetag(){
     if(count>3){
         alert("3개 이상 체크됨");
         //해당 체크박스 취소 코드 짜야함 (안짜져요.. checked=false가 없어!!!!!)
-        
+        obj.checked=false;
+        result=result.pop();
         
     }
 
     // 출력
-    document.getElementById('result').innerText = result;
+    // document.getElementById('result').innerText = result;
     alert(result); //체크된 값 들어가있음
     tag_result=result;
 }
@@ -33,9 +34,9 @@ function choosetag(){
 
 
 function addList() {
-    tag_result='';
+    game_result='';
     const game_name = document.getElementById('game_title').value; //게임명
-    tag_result=game_name;
+    game_result=game_name;
     
     const li = document.createElement("li");
 
@@ -54,12 +55,26 @@ function addList() {
 
     //체크박스 모두 체크 취소 및 text값 안의 입력값 삭제
     //체크박스 모두 체크 취소 좀 짜줘요!!!
+    const checkboxes=document.getElementsByName('tag');
+    checkboxes.forEach((checkbox)=>{
+        checkbox.checked=false;
+    })
     document.getElementById('game_title').value = '';
 
 }
 
 function saveData(){
     //save버튼 클릭시 user_screen에 표시
+    const tagNode = document.createTextNode(tag_result);
+    const textNode = document.createTextNode(game_result);
+
+    // li.appendChild(tagNode);
+    // li.appendChild(textNode);
+
+    // document.getElementById("u_tag_list").appendChild(li)
+    // document.getElementById("u_game_list").appendChild(li)
+
+    // setTimeout("location.href='user_screen.html'", 3000);
 }
 
 const SDK = window.AFREECA.ext;
