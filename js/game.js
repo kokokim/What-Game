e4b15a05e483bfe15b1519d0907f6b6223e6e826
kokokim <111446760+kokokim@users.noreply.github.com>
2023-11-08 23:48:34 +0900

    자네의 할일이라네

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 // bj_screen
+let tag_result=''; //태그 데이터
 function choosetag(){
+    tag_result='';
     var count=0;
     // 선택된 목록 가져오기
     const query = 'input[name="tag"]:checked';
@@ -14,29 +16,42 @@ function choosetag(){
     });
     if(count>3){
         alert("3개 이상 체크됨");
-        //해당 체크박스 취소 코드 짜야함
+        //해당 체크박스 취소 코드 짜야함 (안짜져요.. checked=false가 없어!!!!!)
+        
         
     }
 
     // 출력
     document.getElementById('result').innerText = result;
     alert(result); //체크된 값 들어가있음
-
+    tag_result=result;
 }
 
 
 
-function addList() {
-    const game_name = document.getElementById('game_title').value;
 
+function addList() {
+    const game_name = document.getElementById('game_title').value; //게임명
+    
     const li = document.createElement("li");
 
+    li.setAttribute('tag', tag_result);
     li.setAttribute('id', game_name);
+    
 
+    const tagNode = document.createTextNode(tag_result);
     const textNode = document.createTextNode(game_name);
+
+    li.appendChild(tagNode);
     li.appendChild(textNode);
 
+    document.getElementById("tag_list").appendChild(li)
     document.getElementById("game_list").appendChild(li)
+
+    //체크박스 모두 체크 취소 및 text값 안의 입력값 삭제
+    //체크박스 모두 체크 취소 좀 짜줘요!!!
+    document.getElementById('game_title').value = '';
+
 }
 
 const SDK = window.AFREECA.ext;
